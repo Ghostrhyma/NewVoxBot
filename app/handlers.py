3e02def54b427fa36510d5db7d1d51c6c34f8c2b
Ghostrhyma <denisevichmiha@gmail.com>
2025-11-08 14:52:18 +0300

    test2 send voice

--- app/handlers.py
+++ app/handlers.py
@@ -7,12 +7,31 @@ from aiogram.fsm.state import State, StatesGroup
 
 router = Router()
 
-
+class GetVoice(StatesGroup):
+    file_id = State()
 
 @router.message(Command("start"))
-async def bot_start(message: Message):
+async def bot_start(message: Message, state: FSMContext):
+    await state.set_state(GetVoice.file_id)
+    await message.answer("Запишите голосовое сообщение до 30 секунд")
+    # try:
+    #     file_id = message.voice.file_id
+    #     await message.answer(f"{file_id}")
+    # except Exception as e:
+    #     await message.answer(f"Что то пошло не так: {e}")
+
+
+@router.message(GetVoice.file_id)
+async def get_voice(message: Message, state: FSMContext):
     try:
-        file_id = message.voice.file_id
-        await message.answer(f"{file_id}")
+        if message.voice.duration > 30:
+            await message.answer("Вы превысили допустимую длину сообщения")
+        else:
+            await state.update_data(file_id=message.voice.file_id)
+            id_data = state.get_data()
+            fl_id = id_data["file_id"]
+
+            await message.answer(f"ID гс - {fl_id}")
+            await state.clear()
     except Exception as e:
-        await message.answer(f"Что то пошло не так: {e}")
+        await message.answer(f"Вылезла ошибка: {e}")
