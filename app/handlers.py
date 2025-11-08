6a0e4ca50e21f3d23e7965f92236a65d6a17e88d
Ghostrhyma <denisevichmiha@gmail.com>
2025-11-08 14:17:09 +0300

    test1 get file_id

--- app/handlers.py
+++ app/handlers.py
@@ -1,9 +1,18 @@
 from aiogram import Router
 from aiogram.filters import Command
 from aiogram.types import Message
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
+
 
 router = Router()
 
+
+
 @router.message(Command("start"))
 async def bot_start(message: Message):
-    await message.answer("Давай начнем")
+    try:
+        file_id = message.voice.file_id
+        await message.answer(f"{file_id}")
+    except Exception as e:
+        await message.answer(f"Что то пошло не так: {e}")
---
+++ app/keyboards.py
@@ -0,0 +1,8 @@
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
+
+main_keyboard = ReplyKeyboardMarkup(keyboard=[
+    [KeyboardButton(text="1"),
+     KeyboardButton(text="2"),
+     KeyboardButton(text="3"),
+     KeyboardButton(text="4"),]
+])
